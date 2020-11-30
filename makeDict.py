e82c69a448644981b43ea04d7cd2440cbfbbaaa5
pcm82 <pcm82@cornell.edu>
2020-11-29 23:32:15 -0500

    clean up makeDict.py

--- makeDict.py
+++ makeDict.py
@@ -1,10 +1,12 @@
 import pandas as pd
 
-# Main parser for crossword
-# Returns a dictionary with words and clues
-
-
 def makeDictionary():
+    """
+    :type: 
+    :input: 
+    :rtype: dict = {string: string}
+    :return: dictionary of form {word: clue}
+    """
     data = pd.read_excel("Words,clues.xlsx")
     words = data['Word']
     clues = data['Clue']
@@ -13,11 +15,13 @@ def makeDictionary():
         array.append((words[i], clues[i]))
     return dict(array)
 
-# Takes in a dictionary and sorts it according to length of the key
-# Returns a sorted tuple list [(key, value), ...] with shortest strings first
-
-
 def shortestSort(d):
+    """
+    :type: d: dictionary
+    :input: the word and clue dictionary
+    :rtype: list of tuples
+    :return: sorted tuples (key, value) according to key length
+    """
     keys = list(d.keys())
     keys.sort(key=len)
     array = []
@@ -26,56 +30,44 @@ def shortestSort(d):
         array.append((key, d.get(key)))
     return array
 
-# function that finds out how many characters these two words have in common
-# for heuristics
-
-
 def countIntersections(word1, word2):
+    """
+    :type: word1: string, word2: string
+    :input: two words for which we count the intersections
+    :rtype: integer
+    :return: the number of non-unique characters two words have in common
+    """
     inter = set(word1.lower()).intersection(word2.lower())
     return len(inter)
 
-# Compare function for sorting
-
-
-def compareInt(key1, interDict):
-    return (interDict.get(key1), key1)
-
-# Takes in a dictionary and sorts it according to which words have the most
-# letters in common with the current Word list
-# Returns a sorted tuple list [(key, value), ...] with the words that intersect
-# with the most other words first
-
-
 def mostIntersectionsSort(d, currentWords):
+    """
+    :type: d: dictionary, currentWords: list of words
+    :input: a dictionary and list of words for which sort the dictionary by most intersections with the currentWords
+    :rtype: list of tuples, words and their clues
+    :return: sorted list of words and their clues from the dictionary based on how many times they intersect with words from currentWords
+    """
     interDict = {}
     for key in d:
         count = 0
         for word in currentWords:
             count += countIntersections(key, word)
         interDict[key] = count
-    # makes keys = the list of d's keys, sorted in descending order
     keys = sorted(list(d.keys()), key=lambda x: interDict[x], reverse=True)
     array = []
-    for i in range(len(keys)):
-        key = keys[i]
+    for key in keys:
         array.append((key, d.get(key)))
     return array
 
 
 if __name__ == '__main__':
-    # make the dictionary
-    d = makeDictionary()
+    d = makeDictionary() # make the dictionary
+    print("SORTED KEYS BY LENGTH", shortestSort(d)) # sort the keys according to length
 
-# sort the keys according to length
-    print("SORTED KEYS BY LENGTH", shortestSort(d))
+    # count intersections between two words
+    print("The number of intersections between hello and world is: ", countIntersections("hello", "world"))
+    print("The number of intersections between hello and cello is:", countIntersections("hello", "cello"))
 
-# count intersections between two words
-    print("The number of intersections between hello and world is: ",
-          countIntersections("hello", "world"))
-    print("The number of intersections between hello and cello is:",
-          countIntersections("hello", "cello"))
-
-# sort keys by intersections
+    # sort keys by intersections
     currentWords = ["hello", "world", "cello"]
-    print("SORTED KEYS BY INTERSECTIONS",
-          mostIntersectionsSort(d, currentWords))
+    print("SORTED KEYS BY INTERSECTIONS", mostIntersectionsSort(d, currentWords))
