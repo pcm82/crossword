0888c2aefc4321a179668410d1abc09bdd4dad30
Lindsey Luo <lgl47@cornell.edu>
2020-12-14 17:29:00 -0500

    ok this color looks best

--- GUI.py
+++ GUI.py
@@ -7,7 +7,7 @@ import Crossword
 BOX_SIZE = 50 # pixel size of box
 
 def makeGUI(dim, crossword, words): 
-  sg.theme('DarkRed2')
+  sg.theme('DarkRed')
   text_ids = [None]*dim
 
   # prepare words 
