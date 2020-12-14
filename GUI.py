56f4a708cbd3d1062d4a2344a010480f0a291a64
Lindsey Luo <lgl47@cornell.edu>
2020-12-14 17:28:19 -0500

    less ugly color

--- GUI.py
+++ GUI.py
@@ -7,7 +7,7 @@ import Crossword
 BOX_SIZE = 50 # pixel size of box
 
 def makeGUI(dim, crossword, words): 
-  sg.theme('DarkRed1')
+  sg.theme('DarkRed2')
   text_ids = [None]*dim
 
   # prepare words 
